77d5216e722891a8428eacf857f5080aefeecdc9
Sherin Olivia <sochronicle@gmail.com>
2024-03-27 17:23:32 +0700

    feat: added jobs router to v1

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -5,6 +5,7 @@ import userRouter from './user';
 import authentication from '../../middlewares/authentication';
 import certificateRouter from './certificate';
 import skillRouter from './skill';
+import jobsRouter from './jobs';
 
 const v1Router = express.Router()
 
@@ -13,5 +14,6 @@ v1Router.use('/disability', disabilityRouter);
 v1Router.use('/user', authentication, userRouter);
 v1Router.use('/certificate', authentication, certificateRouter)
 v1Router.use('/skill', authentication, skillRouter)
+v1Router.use('/jobs', jobsRouter)
 
 export default v1Router
