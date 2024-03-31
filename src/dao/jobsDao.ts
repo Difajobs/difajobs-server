904cd8ad73685bffc0215d4ec9bc0b682890debe
Sherin Olivia <sochronicle@gmail.com>
2024-03-31 23:24:00 +0700

    feat: created search job by title and location

--- src/dao/jobsDao.ts
+++ src/dao/jobsDao.ts
@@ -96,7 +96,7 @@ const getCompanyJobsList = async (companyId: number) => {
     }
 }
 
-const searchJobListing = async (searchTitle: string | undefined, searchLocation: string | undefined, limit: number, offset: number) => {
+const searchJobListingByTitle = async (searchTitle: string | undefined, limit: number, offset: number) => {
     try {
         const getJobListing = await prisma.jobs.findMany({
             where: {
@@ -108,7 +108,63 @@ const searchJobListing = async (searchTitle: string | undefined, searchLocation:
                     { description: { 
                         contains: searchTitle 
                         }
-                    },
+                    }
+                ]
+            },
+            include: {
+                company: {
+                    select: {
+                        name: true,
+                        city: true,
+                        logo: true
+                    }
+                },
+                list_ability: {
+                    select: {
+                        ability: {
+                            select: {
+                                name: true
+                            }
+                        }
+                    }
+                },
+                required_skills: {
+                    select: {
+                        skills: {
+                            select: {
+                                name: true
+                            }
+                        }
+                    }
+                }
+
+            },
+            orderBy: {
+                date_posted: 'desc'
+            },
+            take: limit,
+            skip: offset,
+
+        })
+
+        return getJobListing
+    } catch (error: any) {
+        console.error(error);
+        throw new ErrorHandler({
+            success: false,
+            status: error.status,
+            message: error.message,
+        });
+    } finally {
+        await disconnectDB();
+    }
+}
+
+const searchJobListingByLocation = async (searchLocation: string | undefined, limit: number, offset: number) => {
+    try {
+        const getJobListing = await prisma.jobs.findMany({
+            where: {
+                OR: [
                     {company: {
                         city: {
                             contains: searchLocation
@@ -188,4 +244,4 @@ const getAllJobListing = async (limit: number, offset: number) => {
     }
 }
 
-export { getCompanyId, postJob, getCompanyJobsList, searchJobListing, getAllJobListing }
+export { getCompanyId, postJob, getCompanyJobsList, searchJobListingByTitle, searchJobListingByLocation, getAllJobListing }
