4186cd0310c2c67adf5d259c828b81dec4a274c5
Sherin Olivia <sochronicle@gmail.com>
2024-03-31 23:42:50 +0700

    fix: replaced authentication to jobSeekerAuth

--- src/routes/v1/user/index.ts
+++ src/routes/v1/user/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { getJobSeekerProfile, updateJobSeekerData } from '../../../controllers/user';
-import { jobSeekerAuth } from '../../../middlewares/authorization';
 // import { deleteUser } from '../../../controllers/user';
 
 const userRouter = express.Router()
 
-userRouter.get('/', jobSeekerAuth, getJobSeekerProfile)
-userRouter.patch('/personal', jobSeekerAuth, updateJobSeekerData)
+userRouter.get('/', getJobSeekerProfile)
+userRouter.patch('/personal', updateJobSeekerData)
 // userRouter.delete('/deleteaccount', authenticationMiddleware, deleteUser)
 
 export default userRouter;
