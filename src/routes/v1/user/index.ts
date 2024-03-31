d2fce63bf1bf35ad62b4a6cbd376812561beb9ea
Sherin Olivia <sochronicle@gmail.com>
2024-03-31 22:21:01 +0700

    feat: added jobSeekerAuth (job seeker authorization) to user router

--- src/routes/v1/user/index.ts
+++ src/routes/v1/user/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getJobSeekerProfile, updateJobSeekerData } from '../../../controllers/user';
+import { jobSeekerAuth } from '../../../middlewares/authorization';
 // import { deleteUser } from '../../../controllers/user';
 
 const userRouter = express.Router()
 
-userRouter.get('/', getJobSeekerProfile)
-userRouter.patch('/personal', updateJobSeekerData)
+userRouter.get('/', jobSeekerAuth, getJobSeekerProfile)
+userRouter.patch('/personal', jobSeekerAuth, updateJobSeekerData)
 // userRouter.delete('/deleteaccount', authenticationMiddleware, deleteUser)
 
 export default userRouter;
