46e97b4d2f51af40c577df68503e2442f553bcae
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-04-01 16:53:47 +0700

    refac: re-strutured type interface

--- src/utils/type.d.ts
+++ src/utils/type.d.ts
@@ -1,15 +1,66 @@
-// Define the user type
-interface User {
-    id: string;
-    email: string;
-    // Add more properties as needed
-}
-
-// Extend the Request object to include the user property
 declare global {
     namespace Express {
         interface Request {
             user?: User;
         }
     }
+}
+interface User {
+    id: string;
+    email: string;
+}
+
+interface JobSeekerRegistrationData {
+    email: string;
+    password: string;
+    role: string;
+    fullname: string;
+    dob: Date;
+    gender: string;
+    phone_number: string;
+    city: string;
+}
+
+interface RecruiterRegistrationData {
+    email: string;
+    password: string;
+    role: string;
+    name: string;
+    city: string;
+    about: string;
+    logo: string;
+    picture: string;
+}
+
+interface Disability {
+    id: number;
+    category_id: number;
+    name: string;
+    description: string | null;
+}
+
+interface Certificate {
+    id: number;
+    job_seeker_id: number;
+    name: string;
+    description: string | null;
+}
+
+interface LoginInput{
+    email: string,
+    password: string
+}
+
+interface JobCreate {
+    title: string;
+    description: string;
+    gender: string | null;
+    employment_type: string;
+    min_salary: number | null;
+    max_salary: number | null;
+}
+
+interface Skill {
+    id: number;
+    name: string;
 }
