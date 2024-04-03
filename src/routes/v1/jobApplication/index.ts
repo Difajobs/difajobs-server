ddec5a5c6cf3b18fdacccffd39208760070efd1e
Hill-Man-1 <hilmaaansf@gmail.com>
2024-04-03 10:26:28 +0700

    refactor: remove some useless code

--- src/routes/v1/jobApplication/index.ts
+++ src/routes/v1/jobApplication/index.ts
@@ -1,10 +1,5 @@
 import express from 'express';
 import {
-            // getJobApplicationsForJobSeeker, 
-            // getJobApplicationsForCompany, 
-            // updateJobApplicationStatus, 
-            // getJobApplicationsByStatusAndJobSeeker,
-            // getJobApplicationsByStatusAndCompany,
             createJobApplication
         } from '../../../controllers/jobApplication';
 import authentication from '../../../middlewares/authentication';
@@ -12,11 +7,6 @@ import { jobSeekerAuth } from '../../../middlewares/authorization';
 
 const jobApplicationRouter = express.Router()
 
-// jobApplicationRouter.get('/job-seeker/:jobSeekerId', getJobApplicationsForJobSeeker)
-// jobApplicationRouter.get('/company/:companyId', getJobApplicationsForCompany)
-// jobApplicationRouter.put('/status/:applicationId', updateJobApplicationStatus)
-// jobApplicationRouter.get('/job-seeker/:jobSeekerId/status', getJobApplicationsByStatusAndJobSeeker)
-// jobApplicationRouter.get('/company/:companyId/status', getJobApplicationsByStatusAndCompany)
 jobApplicationRouter.post('/', authentication, jobSeekerAuth, createJobApplication)
 
 export default jobApplicationRouter;
