9751d18b66f1ea2512f04db607e441adbc03318a
Sherin Olivia <sochronicle@gmail.com>
2024-04-19 20:07:17 +0700

    feat: created endpoint for get all job application by company and status

--- src/routes/v1/jobApplication/index.ts
+++ src/routes/v1/jobApplication/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { recruiterAuth } from '../../../middlewares/authorization';
-import { getAllJobApplicationByCompany, getJobApplicationsByJobId, getOneJobApplicationByCompany } from '../../../controllers/jobApplication';
+import { getAllJobApplicationByCompany, getAllJobApplicationByCompanyAndStatus, getJobApplicationsByJobId, getOneJobApplicationByCompany } from '../../../controllers/jobApplication';
 
 const jobApplicationRouter = express.Router()
 
 jobApplicationRouter.get('/all', recruiterAuth, getAllJobApplicationByCompany)
 jobApplicationRouter.get('/job/:jobId', recruiterAuth, getJobApplicationsByJobId)
 jobApplicationRouter.get('/application/:jobApplicationId', recruiterAuth, getOneJobApplicationByCompany)
+jobApplicationRouter.get('/status/:status', recruiterAuth, getAllJobApplicationByCompanyAndStatus)
+
 
 export default jobApplicationRouter
