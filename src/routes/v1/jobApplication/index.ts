c0d2d457fb85008c2682d0b6c2d9a41cd5a29de5
Sherin Olivia <sochronicle@gmail.com>
2024-04-19 20:29:32 +0700

    feat: created endpoint for update job application status

--- src/routes/v1/jobApplication/index.ts
+++ src/routes/v1/jobApplication/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { recruiterAuth } from '../../../middlewares/authorization';
-import { getAllJobApplicationByCompany, getAllJobApplicationByCompanyAndStatus, getJobApplicationsByJobId, getOneJobApplicationByCompany } from '../../../controllers/jobApplication';
+import { getAllJobApplicationByCompany, getAllJobApplicationByCompanyAndStatus, getJobApplicationsByJobId, getOneJobApplicationByCompany, updateOneJobApplicationStatus } from '../../../controllers/jobApplication';
 
 const jobApplicationRouter = express.Router()
 
@@ -8,6 +8,6 @@ jobApplicationRouter.get('/all', recruiterAuth, getAllJobApplicationByCompany)
 jobApplicationRouter.get('/job/:jobId', recruiterAuth, getJobApplicationsByJobId)
 jobApplicationRouter.get('/application/:jobApplicationId', recruiterAuth, getOneJobApplicationByCompany)
 jobApplicationRouter.get('/status/:status', recruiterAuth, getAllJobApplicationByCompanyAndStatus)
-
+jobApplicationRouter.put('/application/:jobApplicationId', recruiterAuth, updateOneJobApplicationStatus)
 
 export default jobApplicationRouter
