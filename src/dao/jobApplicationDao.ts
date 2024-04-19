ce33609986a3e47d7f6436f2fa40f06d75b7ab53
Sherin Olivia <sochronicle@gmail.com>
2024-04-19 13:56:21 +0700

    feat: created get all job application by job id and by company

---
+++ src/dao/jobApplicationDao.ts
@@ -0,0 +1,137 @@
+import { disconnectDB, prisma } from "../config/db/dbConnection";
+import ErrorHandler from "../utils/errorHandler";
+
+const getJobApplicationsByJobId = async (companyId : number, jobId : number) => {
+    try {
+        const jobApplication = await prisma.job_application.findMany({
+            where: { 
+                company_id: companyId,
+                job_id: jobId 
+            },
+            include: {
+                answers: true,
+                job_seeker: {
+                    select: {
+                        fullname: true,
+                        city: true,
+                        gender: true,
+                        description: true,
+                        phone_number: true,
+                        user: {
+                            select: {
+                                email: true
+                            }
+                        },
+                        job_seeker_skills: {
+                            select:{
+                                skills: {
+                                    select: {
+                                        name: true,
+                                        
+                                    }
+                                }
+                            }
+                        },
+                        disabilities: {
+                            select: {
+                                disability: {
+                                    select: {
+                                        category: {
+                                            select: {
+                                                name: true
+                                            }
+                                        },
+                                        name: true
+                                    }
+                                }
+                            }
+                        },
+                        certificates: {
+                            select: {
+                                name: true,
+                                description: true
+                            }
+                        },
+                    }
+                }
+            }
+        })
+        return jobApplication
+    } catch (error: any) {
+        console.error(error);
+        throw new ErrorHandler({
+            success: false,
+            status: error.status,
+            message: error.message,
+        });
+    } finally {
+        await disconnectDB();
+    }
+}
+
+const getAllJobApplicationByCompany = async (companyId : number) => {
+    try {
+        const jobApplications = await prisma.job_application.findMany({
+            where: { company_id: companyId },
+            include: {
+                answers: true,
+                job_seeker: {
+                    select: {
+                        fullname: true,
+                        city: true,
+                        gender: true,
+                        description: true,
+                        phone_number: true,
+                        user: {
+                            select: {
+                                email: true
+                            }
+                        },
+                        job_seeker_skills: {
+                            select:{
+                                skills: {
+                                    select: {
+                                        name: true,
+                                        
+                                    }
+                                }
+                            }
+                        },
+                        disabilities: {
+                            select: {
+                                disability: {
+                                    select: {
+                                        category: {
+                                            select: {
+                                                name: true
+                                            }
+                                        },
+                                        name: true
+                                    }
+                                }
+                            }
+                        },
+                        certificates: {
+                            select: {
+                                name: true,
+                                description: true
+                            }
+                        },
+                    }
+                }
+            }
+        })
+        return jobApplications
+    } catch (error: any) {
+        console.error(error);
+        throw new ErrorHandler({
+            success: false,
+            status: error.status,
+            message: error.message,
+        });
+    } finally {
+        await disconnectDB();
+    }
+}
+
+export { getJobApplicationsByJobId, getAllJobApplicationByCompany }
