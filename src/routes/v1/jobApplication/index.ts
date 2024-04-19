685821e0a6ba815bb263cf63eb0dd17d535a7327
Sherin Olivia <sochronicle@gmail.com>
2024-04-19 19:46:17 +0700

    feat: created endpoint for get one job application

--- src/routes/v1/jobApplication/index.ts
+++ src/routes/v1/jobApplication/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { recruiterAuth } from '../../../middlewares/authorization';
-import { getAllJobApplicationByCompany, getJobApplicationsByJobId } from '../../../controllers/jobApplication';
+import { getAllJobApplicationByCompany, getJobApplicationsByJobId, getOneJobApplicationByCompany } from '../../../controllers/jobApplication';
 
 const jobApplicationRouter = express.Router()
 
 jobApplicationRouter.get('/all', recruiterAuth, getAllJobApplicationByCompany)
-jobApplicationRouter.get('/:jobId', recruiterAuth, getJobApplicationsByJobId)
+jobApplicationRouter.get('/job/:jobId', recruiterAuth, getJobApplicationsByJobId)
+jobApplicationRouter.get('/application/:jobApplicationId', recruiterAuth, getOneJobApplicationByCompany)
 
 export default jobApplicationRouter
