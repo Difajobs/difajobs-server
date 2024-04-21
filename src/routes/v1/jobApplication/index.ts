8f626c27942ccabf21da0db28903e0b3961e0467
Sherin Olivia <sochronicle@gmail.com>
2024-04-21 22:11:06 +0700

    Feat: created endpoint for get job applications by status (job seeker pov)

--- src/routes/v1/jobApplication/index.ts
+++ src/routes/v1/jobApplication/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { jobSeekerAuth, recruiterAuth } from '../../../middlewares/authorization';
-import { createJobApplication, getAllJobApplicationByCompany, getAllJobApplicationByCompanyAndStatus, getAllJobApplicationByJobSeeker, getJobApplicationsByJobId, getOneJobApplicationByCompany, getOneJobApplicationByJobSeeker, updateOneJobApplicationStatus } from '../../../controllers/jobApplication';
+import { createJobApplication, getAllJobApplicationByCompany, getAllJobApplicationByCompanyAndStatus, getAllJobApplicationByJobSeeker, getAllJobApplicationByJobSeekerAndStatus, getJobApplicationsByJobId, getOneJobApplicationByCompany, getOneJobApplicationByJobSeeker, updateOneJobApplicationStatus } from '../../../controllers/jobApplication';
 
 const jobApplicationRouter = express.Router()
 
@@ -13,5 +13,6 @@ jobApplicationRouter.put('/application/:jobApplicationId', recruiterAuth, update
 jobApplicationRouter.post('/:jobId', jobSeekerAuth, createJobApplication)
 jobApplicationRouter.get('/by-jobseeker/all', jobSeekerAuth, getAllJobApplicationByJobSeeker)
 jobApplicationRouter.get('/by-jobseeker/:jobApplicationId', jobSeekerAuth, getOneJobApplicationByJobSeeker)
+jobApplicationRouter.get('/by-jobseeker/status/:status', jobSeekerAuth, getAllJobApplicationByJobSeekerAndStatus)
 
 export default jobApplicationRouter
