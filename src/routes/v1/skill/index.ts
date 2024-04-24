157f307d5c822d6de49d9f6258db2fc85fb234a6
Sherin Olivia <sochronicle@gmail.com>
2024-04-24 23:39:08 +0700

    Feat: created endpoints for create required skill and delete required skill

--- src/routes/v1/skill/index.ts
+++ src/routes/v1/skill/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
-import { createNewJobSeekerSkill, deleteJobSeekerSkill, getAllSkill, getJobSeekerSkillList } from '../../../controllers/skill'
+import { createNewJobSeekerSkill, createRequiredSkill, deleteJobSeekerSkill, deleteRequiredSkill, getAllSkill, getJobSeekerSkillList } from '../../../controllers/skill'
+import { jobSeekerAuth, recruiterAuth } from '../../../middlewares/authorization'
 
 const skillRouter = express.Router()
 
 skillRouter.get('/', getJobSeekerSkillList)
 skillRouter.get('/all', getAllSkill)
-skillRouter.post('/', createNewJobSeekerSkill)
-skillRouter.delete('/:jobSeekerSkillId', deleteJobSeekerSkill)
+skillRouter.post('/', jobSeekerAuth, createNewJobSeekerSkill)
+skillRouter.delete('/:jobSeekerSkillId', jobSeekerAuth, deleteJobSeekerSkill)
+
+skillRouter.post('/required-skill/:jobId', recruiterAuth, createRequiredSkill)
+skillRouter.delete('/required-skill/:requiredSkillId', recruiterAuth, deleteRequiredSkill)
 
 export default skillRouter
